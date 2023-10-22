6890a2027da5c9c5fcfb0bfae902896117a55a88
Soberguy <kartik.nigam1712@gmail.com>
2023-10-22 13:16:19 +0530

    patttern 1 code

--- pattern_1.cpp
+++ pattern_1.cpp
@@ -1,20 +1,27 @@
+// Print the pattern: 
+
+// ****
+// ****
+// ****
+// ****
+
 #include<iostream>
 using namespace std;
 
 void pattern(int n){
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) // changes the coloumns
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++) //changes the rows
         {
             cout << "*";
         }
-        cout<<endl;        
+        cout<<endl;  //create next line after each row execution 
     }    
 }
 
 int main(){
-    int n;
-    cin>>n;
+    int n; 
+    cin>>n;  //Takes the input for printing number of rows!
     pattern(n);
 
     return 0;
