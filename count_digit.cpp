0c1c8657022b9b906cb824a170e715e98144190b
Soberguy <kartik.nigam1712@gmail.com>
2023-11-30 08:12:11 +0530

    Count Digit

---
+++ count_digit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n = 7789;
+    int res = n / 10;
+    cout << res << endl;
+    int m = res % 10;
+    cout << m;
+    
+    return 0;
+}
