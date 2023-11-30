5d5e557706b0377908cb8a2719166e114b745102
Soberguy <kartik.nigam1712@gmail.com>
2023-11-30 08:13:19 +0530

    Palindrome Number

---
+++ plaindrome.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    int dup = n;
+    int revNum = 0;
+    while (n > 0){
+        int ld = n % 10;
+        revNum = (revNum*10) + ld;
+        n = n / 10;
+    }
+    cout << revNum << endl;
+    if(revNum == dup)
+    cout << "TRUE";
+    else
+    cout << "FALSE";
+}
