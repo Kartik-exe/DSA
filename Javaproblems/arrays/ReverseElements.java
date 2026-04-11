5367101a87246ba1a122fc696998d1b79cd521d9
Soberguy <kartik.nigam1712@gmail.com>
2026-04-11 12:13:47 +0530

    Reverse an array

---
+++ Javaproblems/arrays/ReverseElements.java
@@ -0,0 +1,30 @@
+package Javaproblems.arrays;
+
+public class ReverseElements {
+
+    public void func(int[] arr, int n){
+
+        int left = 0;
+        int right = n-1;
+
+        while(left<right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args){
+
+        int[] arr = {1, 3, 4};
+        ReverseElements revE = new ReverseElements();
+        revE.func(arr, arr.length);
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
