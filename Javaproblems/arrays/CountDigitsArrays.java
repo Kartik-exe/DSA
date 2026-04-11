2f7df2a33bab845a82a2dcb11fab832835814eae
Soberguy <kartik.nigam1712@gmail.com>
2026-04-11 22:47:40 +0530

    Count number of digits in an array

---
+++ Javaproblems/arrays/CountDigitsArrays.java
@@ -0,0 +1,42 @@
+package Javaproblems.arrays;
+
+import java.util.Scanner;
+
+public class CountDigitsArrays {
+
+    // array -> arr[];
+    // num -> actual numbers in an array
+    // n -> count of quires
+
+    public static int func(int[] arr, int num, int n){
+
+        int Count = 0;
+
+        for(int i = 0; i<n-1; i++){
+            if(arr[i] == num){
+                Count++;
+            }
+        }
+        return Count;
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n-1; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int Q = sc.nextInt(); // number of queries
+        while(Q--> 0){
+            int num = sc.nextInt(); // query number
+            int result = func(arr, num, n);
+            System.out.println(result);
+        }
+
+        sc.close();
+    }
+}
